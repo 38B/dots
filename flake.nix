648a137f75be658bc50083ff9e301eacf57f5a69
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-02 13:38:08 +0000

    initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "a collection of my system, user, and shell delarations";
+
+  inputs = {
+
+    # NixOS Generators
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # NixOS Hardware
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    # Flake Parts
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Home Manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  }
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    nixos-generators,
+    nixos-hardware,
+    home-manager,
+    ...
+  }:
+    (flake-parts.lib.evalFlakeModule
+      {inherit inputs;}
+      {
+        imports = [
+          ./hosts
+        ];
+	systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+        perSystem = {
+          config,
+          inputs',
+          system,
+          ...
+        }: {
+          # make pkgs available to all `perSystem` functions
+          _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
+        };
+	# CI
+        flake.hydraJobs = let
+          inherit (nixpkgs) lib;
+          buildHomeManager = arch:
+            lib.mapAttrs' (name: config: lib.nameValuePair "home-manager-${name}-${arch}" config.activation-script) self.legacyPackages.${arch}.homeConfigurations;
+        in
+          (lib.mapAttrs' (name: config: lib.nameValuePair "nixos-${name}" config.config.system.build.toplevel) self.nixosConfigurations)
+          // (buildHomeManager "x86_64-linux")
+          // (buildHomeManager "aarch64-linux")
+          // (buildHomeManager "aarch64-darwin")
+          // {
+          };
+      })
+    .config
+    .flake;
+}
