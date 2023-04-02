737f7aa528a96bdc4fef20d5e1fa105c5726d87c
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-02 20:16:57 +0000

    openssh not working

--- homes/blob/default.nix
+++ homes/blob/default.nix
@@ -3,7 +3,7 @@
   users.users.blob = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [ "/persist/keystore/blob/id_blob.pub" ];
+    openssh.authorizedKeys.keyFiles = [ "/persist/keystore/blob/id_blob.pub" ];
     passwordFile = "/persist/keystore/blob/passhash";
   };
 }
