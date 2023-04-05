650afb8178bb598280ff0aab885166be811ca86d
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-05 13:27:30 +0000

    add crumb configuration

---
+++ hosts/crumb/configuration.nix
@@ -0,0 +1,107 @@
+{ lib, config, pkgs, ... }:
+{
+  system.stateVersion = "23.05";
+
+  imports = [ 
+    ./hardware-configuration.nix
+    ../../homes/blob
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "crumb";
+  networking.hostId = "eee65be0";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "New_York/America";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "sun12x22";
+    earlySetup = true;
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      # Harden
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+      GatewayPorts = "clientspecified";
+    };
+    # Automatically remove stale sockets
+    extraConfig = ''
+      StreamLocalBindUnlink yes
+    '';
+
+    hostKeys = [{
+      path = "/persist/keystore/sippet/id_crumb";
+      type = "ed25519";
+    }];
+  };
+
+  # Passwordless sudo when SSH'ing with keys
+  security.pam.enableSSHAgentAuth = true;
+
+  nix = {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+    };
+  };
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  zramSwap = {
+    enable = true;
+    swapDevices = 1;
+  };
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
+  ];
+  
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5 {
+    enable = true;
+  };
+
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    file
+    wget
+    curl
+    btop
+    lsof
+    tmux
+    rsync
+    git
+    tig
+    pinentry
+    pinentry-curses
+    cryptsetup
+    gnupg
+    which
+    tomb
+
+    libsForQt5.bismuth
+  ];
+
+}
---
+++ hosts/crumb/hardware-configuration.nix
@@ -0,0 +1,69 @@
+{ lib, config, pkgs, ...}:
+{
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "i915" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [ "elevator=none" ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.devNodes = "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b444a494a69d2-part3";
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    zfs rollback -r zroot/ephemeral/slash@blank
+  '';
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "zroot/ephemeral/slash";
+      fsType = "zfs";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/0BBD-D5CF";
+      fsType = "vfat";
+    };
+
+  fileSystems."/nix" =
+    { device = "zroot/ephemeral/nix";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home" =
+    { device = "zroot/eternal/home";
+      fsType = "zfs";
+    };
+
+  fileSystems."/home/muck" =
+    { device = "zroot/eternal/homes/muck";
+      fsType = "zfs";
+    };
+
+  fileSystems."/persist" =
+    { device = "zroot/eternal/persist";
+      fsType = "zfs";
+      neededForBoot = true;
+    };
+
+  zramSwap = {
+    enable = true;
+    swapDevices = 1;
+  };
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  powerManagement.cpuFreqGovernor = "powersave";
+
+  hardware.opengl.enable = true;
+  hardware.cpu.intel.updateMicrocode = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    vaapiIntel
+    libvdpau-va-gl
+    intel-media-driver
+  ];
+
+  environment.variables = {
+    VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
+  };
+
+  services.thermald.enable = true;  
+}
