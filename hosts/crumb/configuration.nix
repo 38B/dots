6c018e3de2df4b824d32d7e8b46945ade362ede7
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-05 20:56:42 +0000

    add pipewire for sound

--- hosts/crumb/configuration.nix
+++ hosts/crumb/configuration.nix
@@ -83,6 +83,15 @@
     };
   };
 
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     neovim
     file
