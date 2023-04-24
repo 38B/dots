6608f582e199bc583bfb709bf751111301a6ae1a
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-24 10:04:39 -0400

    turn off wifi card on boot

--- hosts/sippet/configuration.nix
+++ hosts/sippet/configuration.nix
@@ -92,6 +92,7 @@
   };
 
   networking.hostId = "3f871983";
+  networking.wireless.enable = false;
   boot.supportedFilesystems = [ "zfs" "ntfs" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelParams = [ "elevator=none" "radeon.si_support=0" "amdgpu.si_support=1" ];
