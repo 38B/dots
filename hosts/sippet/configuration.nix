8c8344a98bcd354377d84e21e4ec6c20e8f1aca2
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-25 10:39:59 -0400

    handle lid closing on laptop server

--- hosts/sippet/configuration.nix
+++ hosts/sippet/configuration.nix
@@ -121,6 +121,11 @@
     targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
     services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
   };
+  
+  services.logind.extraConfig = ''
+    HandleLidSwitch=ignore
+    HandleLidSwitchExternalPower=ignore
+  '';
 
 }
 
