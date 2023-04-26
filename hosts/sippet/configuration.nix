c6cbb92e39ae9e447d7984c415b73fd7977eece0
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-04-26 09:41:43 -0400

    unlock zfs encryption remotely

--- hosts/sippet/configuration.nix
+++ hosts/sippet/configuration.nix
@@ -135,7 +135,9 @@
         };
       };
       kernelModules = [ "amdgpu" "r8169" ];
-      preLVMCommands = lib.mkBefore 400 "sleep 1";
+      preLVMCommands = lib.mkBefore ''
+        sleep 1
+      '';
       postDeviceCommands = lib.mkAfter ''
         zfs rollback -r sippet-os/ephemeral/slash@blank
       '';
