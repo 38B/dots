f347c9dffd0d0ef5bf536a19a93d232989ad2ca0
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-06-10 15:36:14 -0400

    configHome

--- homes/speck/crumb.nix
+++ homes/speck/crumb.nix
@@ -21,7 +21,7 @@
   xdg = {
     enable = true;
     cacheHome = "${config.home.homeDirectory}/.local/run";
-    configHome = "${}/.local/etc";
+    configHome = "$HOME/.local/etc";
   };
 
   home = {
