104892c8f6d46ffe54494ad0a1c8255fc272a930
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-06-16 09:36:42 -0400

    nvim module

--- modules/home-manager/editors/nvim.nix
+++ modules/home-manager/editors/nvim.nix
@@ -14,6 +14,19 @@
       opt.tabstop = 2
       opt.softtabstop = 2
 
+      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+        if not vim.loop.fs_stat(lazypath) then
+          vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath,
+          })
+        end
+      vim.opt.rtp:prepend(lazypath)
+
       require("lazy").setup({
         { "folke/which-key.nvim", 
            config = function()
