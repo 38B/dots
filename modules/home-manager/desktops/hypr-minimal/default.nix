17f649799a692022136188623ff0ba3aa61c7746
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-07-05 20:11:48 -0400

    hypr rewrite

---
+++ modules/home-manager/desktops/hypr-minimal/default.nix
@@ -0,0 +1,21 @@
+{ lib, config, pkgs, hyprland, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+  };
+  
+  home.packages = with pkgs; [
+    sirula
+    mc
+    hyprpaper
+  ];
+
+  xdg.configFile.hypr = {
+    source = ./hypr;
+    recursive = true;
+  };
+
+  xdg.dataFile."wallpapers/nixos-ice.png" = {
+    source = ./nixos-ice.png;
+  }
+}
