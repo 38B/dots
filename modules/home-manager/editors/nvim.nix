71019cf3c61d983459fe4b612ddc2cf44297a169
Thirty Eighth and Brunswick <86792483+38B@users.noreply.github.com>
2023-07-14 08:19:43 -0400

    chatgpt shortcuts

--- modules/home-manager/editors/nvim.nix
+++ modules/home-manager/editors/nvim.nix
@@ -106,7 +106,7 @@
                    scroll_up = "<C-u>",
                    scroll_down = "<C-d>",
                    toggle_settings = "<C-o>",
-                   new_session = "<C-s>",
+                   new_session = "<C-n>",
                    cycle_windows = "<Tab>",
                    select_session = "<Space>",
                    rename_session = "r",
